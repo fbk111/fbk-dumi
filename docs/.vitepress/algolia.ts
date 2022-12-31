fc5bf82efa38f860c450ea3d481a035d43d74ced
fbk111 <2290291854@qq.com>
2022-12-31 10:26:04 +0800

    提交appid

--- docs/.vitepress/algolia.ts
+++ docs/.vitepress/algolia.ts
@@ -1,7 +1,7 @@
 export default {
-  appId: 'RDDXUZAIF1',
-  apiKey: '9302db06c2d4f1d7b7d243e7e8d877d7',
-  indexName: 'chodocs',
+  appId: '90CL534GQO',
+  apiKey: '03638cbd64562fa06dc7a92a92eca9d0',
+  indexName: 'Fcsummer',
   placeholder: '请输入关键词',
   buttonText: '搜索',
 }
